9f84bbed6066cb884d9c3ba56e56d5b04a19fade
100beatskir <107321124+100beatskir@users.noreply.github.com>
2022-06-11 11:48:16 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var box;
+
+function setup() {
+  createCanvas(600,600);
+  box = createSprite(300,300,30,30);
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+
+if(keyIsDown(RIGHT_ARROW)){
+  box.position.x = box.position.x + 5;
+}
+
+if(keyIsDown(LEFT_ARROW)){
+  box.position.x = box.position.x - 5;
+}
+
+if(keyIsDown(UP_ARROW)){
+  box.position.y = box.position.y - 5;
+}
+
+if(keyIsDown(DOWN_ARROW)){
+  box.position.y = box.position.y + 5;
+}
+}
+
